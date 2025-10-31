df78715545385ef760c8e41a7de647759768bb6b
kotiko <megasuslik666@gmail.com>
2025-10-31 17:20:14 +0200

    add files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
-
-export default withNuxt(
-  // Your custom configs here
-)
+module.exports = {
+	extends: [
+		'eslint:recommended',
+		'plugin:vue/vue3-recommended',
+		'plugin:prettier/recommended',
+	],
+	rules: {
+		quotes: ['error', 'single'],
+	},
+}
